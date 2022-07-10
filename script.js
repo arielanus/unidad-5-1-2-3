cdd33a28c216295a6401ee33544136e03d995217
Ariel <arielanus1980@yahoo.com.ar>
2022-07-10 12:10:53 -0300

    tercer ejercicio

---
+++ script.js
@@ -0,0 +1,4 @@
+var primerNota =Number(prompt("Ingrese un numero"))
+var segundaNota =Number(prompt("Ingrese un numero"))
+var promedio = (primerNota + segundaNota) / 2
+if (primerNota > 7 && segundaNota > 7 && promedio > 7) {alert("Materia aprobada")} 
